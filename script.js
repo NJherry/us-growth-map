91d184de75ffbd099c4c0cd71d7a2211381ad741
NJherry <97069700+NJherry@users.noreply.github.com>
2025-11-13 23:46:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -130,4 +130,10 @@ loadMap(1783);
 
 document.getElementById("yearSelect").addEventListener("change", function() {
   loadMap(parseInt(this.value));
+    
+document.getElementById("yearSelect").addEventListener("change", function() {
+    let year = parseInt(this.value);
+    loadMap(year);
+    document.getElementById("yearInfo").innerText = yearDetails[year];
+});    
 });
